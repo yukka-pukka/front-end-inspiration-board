fe26972d2cbc7aad3e475f75129d3b7cd7cc36bf
Priscille Gwet <ngomakak@gmail.com>
2021-06-29 12:02:14 -0700

    Created a components folder and add Board.js,Card.js, CardList.js, NewBoardForm.js, NewCardForm.js

---
+++ src/components/NewBoardForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const NewBoardForm = (props) => {
+
+    const [formFields, setFormFields] = useState({
+        title: "",
+        owner: ""
+    })
+
+
+    const onTitleChange = (event) => {
+        setFormFields({
+            ...formFields,
+            title: event.target.value
+        })
+    };
+
+    const onOwnerChange = (event) => {
+        setFormFields({
+            ...formFields,
+            owner: event.target.value
+        })
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        props.addBoardCallback({
+            titleData: formFields.title,
+            ownerData: formFields.owner
+        });
+
+        setFormFields({
+            title: "",
+            owner: ""
+        })
+    };
+
+
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <div>
+                <label htmlFor="title">Title</label>
+                <input
+                    type="text"
+                    name="title"
+                    value={formFields.title}
+                    onChange={onTitleChange} />
+            </div>
+            <div>
+                <label htmlFor="owner">Owner's Name</label>
+                <input
+                    type="text"
+                    name="owner"
+                    value={formFields.owner}
+                    onChange={onOwnerChange} />
+            </div>
+            <p>Preview: {formFields.title}</p>
+            <div>
+                <input type="submit" value="submit"/>
+            </div>
+        </form>
+    )
+}
+
+NewBoardForm.propTypes = {
+    addBoardCallback: PropTypes.func.isRequired
+};
+
+export default NewBoardForm;
